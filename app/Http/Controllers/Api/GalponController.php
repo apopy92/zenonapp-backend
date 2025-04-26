34b545b7df4ad15d28a7b9c9f451360d5507ed14
apopy92 <apopy92@gmail.com>
2025-04-26 17:39:33 -0300

    Fix: corregido modelo Galpon en GalponController

--- app/Http/Controllers/Api/GalponController.php
+++ app/Http/Controllers/Api/GalponController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Galpones;
+use App\Models\Galpon;
 
 class GalponController extends Controller
 {
     public function index()
     {
-        return Galpones::select('id', 'nombre')->get();
+        return Galpon::select('id', 'nombre')->get();
     }
 }
